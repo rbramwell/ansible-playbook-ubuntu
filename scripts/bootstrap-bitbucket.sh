0f60fac3000962b401a03d6b272f7f72f7f6ce43
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-07-23 18:17:00 +0800

    ./scripts/bootstrap-roles.sh && ./scripts/update-tests.sh

--- scripts/bootstrap-bitbucket.sh
+++ scripts/bootstrap-bitbucket.sh
@@ -48,6 +48,7 @@ perl -i -p -e "s/^#(bitbucket_user):.*/\1: \"{{ apache2_vhosts_user }}\"/g" $TMP
 perl -i -p -e "s/^#(mysql_connector_java_dest):.*/\1: \"\/usr\/share\/bitbucket\/lib\"/g" $TMP_VARS
 perl -i -p -e "s/^#(mysql_connector_java_user):.*/\1: \"{{ apache2_vhosts_user }}\"/g" $TMP_VARS
 perl -i -p -e "s/^#(mysql_port):.*/\1: \"3306\"/g" $TMP_VARS
+perl -i -p -e "s/^#(mysql_root_pass):.*/\1: \"$PASSWD\"/g" $TMP_VARS
 perl -i -p -e "s/^#(mysql_vhosts_collation):.*/\1: \"utf8_bin\"/g" $TMP_VARS
 perl -i -p -e "s/^#(mysql_vhosts_encoding):.*/\1: \"utf8\"/g" $TMP_VARS
 perl -i -p -e "s/^#(mysql_vhosts_id):.*/\1: \"{{ apache2_vhosts_id }}\"/g" $TMP_VARS
