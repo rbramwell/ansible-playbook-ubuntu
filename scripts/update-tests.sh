d964bfaabe3de81926fd97606d0a5a69763c3978
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-08-10 15:01:40 +0800

    ansible/ansible#16956: {host,group}_vars need to be adjacent to the inventory FILE on ansible 2.1.1+

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -2,11 +2,11 @@
 
 set -o xtrace
 
-rm -rf host_vars/ubuntu.aio
+rm -rf inventory/host_vars/ubuntu.aio
 rm -rf inventory/ubuntu.aio
 
 scripts/bootstrap-inventory.sh
 scripts/bootstrap-ubuntu.sh
 
-cat host_vars/ubuntu.aio | egrep -v -e '^^#\w' | perl -p -e "s/^(apt_upgrade):.*/\1: \"no\"/g" > tests/group_vars/all
+cat inventory/host_vars/ubuntu.aio | egrep -v -e '^^#\w' | perl -p -e "s/^(apt_upgrade):.*/\1: \"no\"/g" > tests/group_vars/all
 cat inventory/ubuntu.aio | perl -p -e "s/^.*127.0.0.1/localhost.localdomain\tansible_connection=local/g" > tests/hosts
