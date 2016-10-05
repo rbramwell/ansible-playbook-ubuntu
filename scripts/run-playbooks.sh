6684537353e364a5b7923e73f27916644f10cfc9
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-10-05 17:41:37 +0800

    update scripts/run-playbooks.sh so support multi-level inventory structure

--- scripts/run-playbooks.sh
+++ scripts/run-playbooks.sh
@@ -2,4 +2,12 @@
 
 set -o xtrace
 
-ansible-playbook -i inventory/ubuntu.aio/hosts.aio playbooks/setup-everything.yml
+find inventory/ -maxdepth 2 -depth -type f -name 'hosts*' -printf '%d\t%p\n' | sort -nk1 | cut -f2- | while read line
+do
+    ansible-playbook -i $line playbooks/setup-hosts.yml playbooks/setup-infrastructure.yml
+done
+
+find inventory/ -maxdepth 9 -depth -type f -name 'hosts*' -printf '%d\t%p\n' | sort -nk1 | cut -f2- | while read line
+do
+    ansible-playbook -i $line playbooks/setup-service.yml
+done
