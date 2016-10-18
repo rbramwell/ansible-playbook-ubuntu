81b37e42770d86347573b8bebbf4be2f1336f679
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-10-18 10:11:58 +0800

    only run inventory with filename hosts

--- scripts/run-playbooks.sh
+++ scripts/run-playbooks.sh
@@ -2,12 +2,12 @@
 
 set -o xtrace
 
-find inventory/ -maxdepth 2 -depth -type f -name 'hosts*' -printf '%d\t%p\n' | sort -nk1 | cut -f2- | while read line
+find inventory/ -maxdepth 2 -depth -type f -name 'hosts' -printf '%d\t%p\n' | sort -nk1 | cut -f2- | while read line
 do
     ansible-playbook -i $line playbooks/setup-hosts.yml playbooks/setup-infrastructure.yml
 done
 
-find inventory/ -maxdepth 9 -depth -type f -name 'hosts*' -printf '%d\t%p\n' | sort -nk1 | cut -f2- | while read line
+find inventory/ -maxdepth 9 -depth -type f -name 'hosts' -printf '%d\t%p\n' | sort -nk1 | cut -f2- | while read line
 do
     ansible-playbook -i $line playbooks/setup-service.yml
 done
