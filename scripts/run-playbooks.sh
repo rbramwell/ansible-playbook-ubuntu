58b56aef169d7a8b8dd3e6f6d1fead43f1ac5f32
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-12-29 22:14:26 +0800

    pass extra arguments to scripts/run-playbooks.sh

--- scripts/run-playbooks.sh
+++ scripts/run-playbooks.sh
@@ -4,10 +4,10 @@ set -o xtrace
 
 find inventory/ -maxdepth 2 -depth -type f -name 'hosts' -printf '%d\t%p\n' | sort -nk1 | cut -f2- | while read line
 do
-    ansible-playbook -i $line playbooks/setup-hosts.yml playbooks/setup-infrastructure.yml
+    ansible-playbook -i $line playbooks/setup-hosts.yml playbooks/setup-infrastructure.yml $@
 done
 
 find inventory/ -maxdepth 9 -depth -type f -name 'hosts' -printf '%d\t%p\n' | sort -nk1 | cut -f2- | while read line
 do
-    ansible-playbook -i $line playbooks/setup-service.yml
+    ansible-playbook -i $line playbooks/setup-service.yml $@
 done
